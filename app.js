f8accc4287a7479ee0fc626cf5ccf34e65c3701b
Joshua Yannix <joshuayannix@gmail.com>
2020-08-12 00:01:53 -0500

    begin and end slideshow circle

--- app.js
+++ app.js
@@ -13,8 +13,30 @@ let counter = 0;
 
 nextBtn.addEventListener('click', () => {
   counter++
+  carousel()
 })
 
 prevBtn.addEventListener('click', () => {
   counter--
+  carousel()
 })
+
+// function that grabs value of counter, 
+// and then adds translateX value to all the slides
+
+const carousel = () => {
+  
+  // circle back to beginining of slideshow if you get to end
+  if(counter === slides.length){
+    counter = 0;
+  }
+
+  if(counter < 0){
+    counter = slides.length - 1
+  }
+
+  // translate will move EVERY slide over to the left or right, depending on the counter
+  slides.forEach(slide => {
+    slide.style.transform = `translateX(-${counter * 100}%)`
+  })
+}
